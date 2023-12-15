4392a694ff4ee59837fe50f081c65e7a40a1650b
oscarfr96 <oscar.fm.96@gmail.com>
2023-12-15 13:57:15 +0100

    Project 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Project01';
+  @HostBinding('class.container-custom') containerClass = true;
+  title = 'Porject 1 - Oscar Fraile';
+
+  items = [
+    { name: 'Item 1' },
+    { name: 'Item 2' },
+    { name: 'Item 3' }
+  ];
+
+  agregarItem(itemForm: any) {
+    const nuevoItemNombre = itemForm.value.itemName;
+    if (nuevoItemNombre) {
+      this.items.push({ name: nuevoItemNombre });
+      // Limpiar el campo de entrada después de agregar el ítem
+      itemForm.resetForm();
+    }
+  }
+
 }
